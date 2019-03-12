e231f083d0ad30b243b963710a92f181622a3994
DisableGraphics <48135147+DisableGraphics@users.noreply.github.com>
2019-03-12 21:42:40 +0100

    Add files via upload

---
+++ VisualPhoton/VisualPhoton/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace VisualPhoton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openbut = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 394);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openbut
+            // 
+            this.openbut.BackColor = System.Drawing.Color.Gray;
+            this.openbut.Font = new System.Drawing.Font("Segoe Script", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openbut.Location = new System.Drawing.Point(13, 13);
+            this.openbut.Name = "openbut";
+            this.openbut.Size = new System.Drawing.Size(75, 23);
+            this.openbut.TabIndex = 1;
+            this.openbut.Text = "Open";
+            this.openbut.UseVisualStyleBackColor = false;
+            this.openbut.Click += new System.EventHandler(this.openbut_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "All photos |*png; *jpg; *bmp; *gif; *webp; *tif; *tga; *ico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openbut);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openbut;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ VisualPhoton/VisualPhoton/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualPhoton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void openbut_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Bitmap.FromFile(open.FileName);
+                if (pictureBox1.Image.HorizontalResolution < pictureBox1.Size.Width)
+                {
+                    if (pictureBox1.Image.VerticalResolution < pictureBox1.Size.Height)
+                    {
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                        pictureBox1.Image = Bitmap.FromFile(open.FileName);
+
+                    }
+                    if (pictureBox1.Image.VerticalResolution >= pictureBox1.Size.Height)
+                    {
+                        pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+
+                        pictureBox1.Image = Bitmap.FromFile(open.FileName);
+
+                    }
+
+                }
+                if (pictureBox1.Image.HorizontalResolution >= pictureBox1.Size.Width)
+                {
+                    if (pictureBox1.Image.VerticalResolution < pictureBox1.Size.Height)
+                    {
+                        pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                        pictureBox1.Image = Bitmap.FromFile(open.FileName);
+
+                    }
+                    if (pictureBox1.Image.VerticalResolution >= pictureBox1.Size.Height)
+                    {
+                        pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                        pictureBox1.Image = Bitmap.FromFile(open.FileName);
+
+                    }
+
+                }
+
+            } 
+        }
+    }
+}
